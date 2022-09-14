66088f4660967e937a3159b5db49bd5164f84e6e
viola134 <113242400+viola134@users.noreply.github.com>
2022-09-14 12:31:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $b = 37.7;
 $a = rand(360, 410)/10;
+echo( $a );
 echo ($a>$b? "<span style = \"color: red;\">Болен! </span>" : ($a == $b ?
 "Что-то нездоровится..." : "<span style = \" color: blue;\"> Здоров!</span>"));
